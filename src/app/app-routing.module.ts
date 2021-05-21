ce65084ddd8114bbf85717d3ce5345d262270f10
Speedj1234 <jonas.moens@ymail.com>
2021-05-21 15:52:15 +0200

    add auth

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,20 +3,22 @@ import { RouterModule, Routes } from '@angular/router';
 import { AuthComponent } from './components/auth/auth.component';
 import { CreateUserComponent } from './components/create-user/create-user.component';
 import { MainComponent } from './components/main/main.component';
+import { ProductsComponent } from './components/products/products.component';
 import{UsersComponent} from './components/users/users.component'
 
 const routes: Routes = [
 
    {path: '', redirectTo: 'home', pathMatch: 'full'},
-   {path : 'auth', component: AuthComponent},
+  
 
    {path: 'create-user', component: CreateUserComponent},
    {path: 'users', component: UsersComponent},
+   {path: 'products', component: ProductsComponent},
 
 
 
-   {path: 'home', component: MainComponent}
-
+   {path: 'home', component: MainComponent},
+   {path : 'auth', component: AuthComponent}
 ];
 
 @NgModule({
