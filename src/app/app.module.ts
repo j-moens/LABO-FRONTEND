09c31940e28d276a9457751616fd3da029878252
Speedj1234 <jonas.moens@ymail.com>
2021-05-25 14:03:02 +0200

    test

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,3 +1,42 @@
+// Material
+
+import { MatAutocompleteModule } from "@angular/material/autocomplete";
+import { MatButtonModule } from "@angular/material/button";
+import { MatButtonToggleModule } from "@angular/material/button-toggle";
+import { MatCheckboxModule } from "@angular/material/checkbox";
+import { MatToolbarModule } from "@angular/material/toolbar";
+import { MatTooltipModule } from "@angular/material/tooltip";
+import { MatCardModule } from "@angular/material/card";
+import { MatChipsModule } from "@angular/material/chips";
+import { MatDatepickerModule } from "@angular/material/datepicker";
+import { MatDialogModule } from "@angular/material/dialog";
+import { MatExpansionModule } from "@angular/material/expansion";
+import { MatFormFieldModule } from "@angular/material/form-field";
+import { MatGridListModule } from "@angular/material/grid-list";
+import { MatIconModule } from "@angular/material/icon";
+import { MatInputModule } from "@angular/material/input";
+import { MatListModule } from "@angular/material/list";
+import { MatMenuModule } from "@angular/material/menu";
+// import {  MatNativeDateModule} from "@angular/material";
+import { MatPaginatorModule } from "@angular/material/paginator";
+import { MatProgressBarModule } from "@angular/material/progress-bar";
+import { MatProgressSpinnerModule } from "@angular/material/progress-spinner";
+import { MatRadioModule } from "@angular/material/radio";
+// import {  MatRippleModule} from "@angular/material/r";
+import { MatSelectModule } from "@angular/material/select";
+import { MatSidenavModule } from "@angular/material/sidenav";
+import { MatSliderModule } from "@angular/material/slider";
+import { MatSlideToggleModule } from "@angular/material/slide-toggle";
+import { MatSnackBarModule } from "@angular/material/snack-bar";
+import { MatSortModule } from "@angular/material/sort";
+import { MatTableModule } from "@angular/material/table";
+import { MatTabsModule } from "@angular/material/tabs";
+import { MatStepperModule } from "@angular/material/stepper";
+
+
+//
+
+
 import {HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
@@ -12,6 +51,8 @@ import { FourhofourComponent } from './components/fourhofour/fourhofour.componen
 import { MainComponent } from './components/main/main.component';
 import { UsersComponent } from './components/users/users.component';
 import { ProductsComponent } from './components/products/products.component';
+import { AdminGuard, AuthGuard } from './services/auth-guard.service';
+
 
 
 import { UsersService } from './services/users.service';
@@ -19,6 +60,15 @@ import { ServerService } from './services/server.service';
 import { AuthService } from './services/auth.service';
 import { UsersCommonService } from './services/users-common.service';
 import { ProductsService } from './services/products.service';
+import { JwtModule } from '@auth0/angular-jwt';
+import { ProfileComponent } from './components/profile/profile.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ChangePasswordComponent } from './components/change-password/change-password.component';
+
+
+export function tokenGetter() {
+  return sessionStorage.getItem('id_token');
+}
 
 
 @NgModule({
@@ -29,21 +79,39 @@ import { ProductsService } from './services/products.service';
     FourhofourComponent,
     MainComponent,
     UsersComponent,
-    ProductsComponent
+    ProductsComponent,
+    ChangePasswordComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
     FormsModule, 
-    ReactiveFormsModule
+    ReactiveFormsModule,
+       // material
+       MatButtonModule, MatCheckboxModule, MatFormFieldModule, MatInputModule, MatListModule,
+       MatTableModule, MatPaginatorModule, MatSortModule, MatIconModule,
+       MatSlideToggleModule, MatDialogModule, MatSnackBarModule, MatRadioModule,
+       MatTabsModule,
+       //
+    JwtModule.forRoot({
+      config: {
+          tokenGetter: tokenGetter,
+          allowedDomains: ["localhost:8000"]
+      }}),
+    BrowserAnimationsModule,
+
+ 
   ],
   providers: [
     UsersService,
     ServerService,
+    UsersService,
     AuthService,
     UsersCommonService,
-    ProductsService
+    ProductsService,
+    AuthGuard,
+    AdminGuard
 
 
   ],
