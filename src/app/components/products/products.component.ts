be8d620a59dd5308ccc16a1350f527b18d4c6316
Speedj1234 <jonas.moens@ymail.com>
2021-06-01 15:54:03 +0200

    view products ok

--- src/app/components/products/products.component.ts
+++ src/app/components/products/products.component.ts
@@ -1,22 +1,31 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { Product } from 'src/app/models/product.model';
-import{ProductsService} from'../../services/products.service'
-import {ModelService} from '../../services/model.service'
+import{ProductsService} from'../../services/products.service';
+import {ModelService} from '../../services/model.service';
+import{BrandService} from'../../services/brand.service';
 import { Model } from 'src/app/models/model.model';
+import{Brand} from 'src/app/models/brand.model'
+import { subscribeOn } from 'rxjs/operators';
 
-
+// classe productmodel : pour afficher le nom des modèles
 class ProductModel
 {
   public product: Product; 
   public model : Model;
+  public brand: Brand;
 
-  constructor( product : Product, model: Model)
+  constructor( product : Product, model: Model, brand: Brand)
   {
     this.product= product;
     this.model = model;
+    this.brand = brand;
   }
 }
+
+
+
+
 
 @Component({
   selector: 'app-products',
@@ -24,32 +33,43 @@ class ProductModel
   styleUrls: ['./products.component.scss']
 })
 export class ProductsComponent implements OnInit {
-  public productsList: ProductModel[]=[];
+  public productsList: ProductModel[]  = [];
 
-  constructor(private productsService: ProductsService, private modelService: ModelService, private router: Router) { }
+
+
+  constructor(private productsService: ProductsService, private modelService: ModelService, private brandService : BrandService, private router: Router) { }
 
   ngOnInit(): void {
   
      this.productsService.getAll().subscribe(products =>
       {
-         products.forEach(p=>
+         products.forEach
+
+         (p=>
           {
+         
             this.modelService.getOneById(p.fk_model).subscribe(model =>
               {
-                if (model){
-                  this.productsList.push(new ProductModel(p, model))
-                }else{
-                  this.productsList.push(new ProductModel(p, new Model({})))
+                if(model){
+                  this.brandService.getOneById(model.fk_brand).subscribe
+                  (brand =>
+                    {
+                    
+                      if (model && brand){
+                        this.productsList.push (new ProductModel(p, model, brand))
+                       // console.log(this.productsList)
+                      }
+                    })          
+                  }else
+                  {
+                    this.productsList.push(new ProductModel(p, new Model({}), new Brand({})))
+                  }
                 }
-                
+            )
               })
-            
-          }
-          )
-       
+
        })
-       console.log(this.productsList);
-    
-       
+      //console.log(this.productsList)      
      }
+     
 }
